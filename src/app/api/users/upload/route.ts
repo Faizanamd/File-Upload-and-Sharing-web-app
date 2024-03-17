2cb6c4b96b8e6c8da7ace0c2c30bb53eac0f1fa1
FZNAhmad <faiz818791@gmail.com>
2024-03-17 22:21:08 +0530

    ;sf

--- src/app/api/users/upload/route.ts
+++ src/app/api/users/upload/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: NextRequest) {
     }
     const byteData = await file.arrayBuffer();
     const buffer = Buffer.from(byteData);
-    const path = `./public/${file.name}`;
+    const path = `https://file-upload-and-sharing-web-app.vercel.app/${file.name}`;
     await writeFile(path, buffer);
 
     // Save file information to MongoDB
