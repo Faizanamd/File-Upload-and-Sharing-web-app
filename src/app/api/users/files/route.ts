a64bf2776aad2f5c8dd29892f3e76817e1e53c5b
FZNAhmad <faiz818791@gmail.com>
2024-03-17 22:27:41 +0530

    changes in file route

--- src/app/api/users/files/route.ts
+++ src/app/api/users/files/route.ts
@@ -3,17 +3,20 @@ import { getUserId } from "@/helpers/getuserid";
 import FileModel from "@/models/File.Model";
 import { NextRequest, NextResponse } from "next/server";
 connectToMongoose();
-export async function GET(request:NextRequest) {
-    
+
+
+export async function GET(request: NextRequest) {
     try {
-       const userId = await getUserId(request);
-       const files = await FileModel.find({userId:userId});
-    //    console.log("files",files);
-       if(!files){
-        return NextResponse.json({status:false, message:"Not founds"});
-       }
-       return NextResponse.json({status:true, message:"Your files", files:files}); 
-    } catch (error:any) {
-        return NextResponse.json({status:false, message:error.message})
+        const userId = await getUserId(request);
+        const files = await FileModel.find({ userId: userId });
+
+        if (!files || files.length === 0) {
+            return NextResponse.json({ status: false, message: "Files not found" });
+        }
+
+        return NextResponse.json({ status: true, message: "Your files", files: files });
+    } catch (error: any) {
+        console.error("Error fetching files:", error);
+        return NextResponse.json({ status: false, message: "An error occurred while fetching files" });
     }
 }
